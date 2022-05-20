d4292d1f7f61f7f8ac8b18105be4bbfbe087a4e0
21yuyu99 <yujeong0720@gmail.com>
2022-05-20 17:29:41 +0900

    wit_3

---
+++ 오유정/wit_3/src/App.js
@@ -0,0 +1,15 @@
+// import logo from './logo.svg';
+// import './App.css';
+import Header from "./Header.js"
+import Body from "./Body.js"
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      <Body/>
+    </div>
+  );
+}
+
+export default App;
