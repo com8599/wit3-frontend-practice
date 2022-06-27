ae6001a86dc7790ef942084dccadaf5f1dd1ea1d
김태헌 <57055730+Jake-huen@users.noreply.github.com>
2022-06-27 15:38:09 +0900

    comments

--- 김태헌/blog/blog/src/Posts.js
+++ 김태헌/blog/blog/src/Posts.js
@@ -1,32 +1,33 @@
 import React, { useState } from "react";
 import { useEffect } from "react";
+import "./Posts.css";
 const Posts = ({ post }) => {
   const [comments, setComments] = useState([]);
   useEffect(() => {
     const initcommentData = async () => {
       const response = await fetch(
-        "https://jsonplaceholder.typicode.com/comments?postId={post.id}"
+        `https://jsonplaceholder.typicode.com/comments?postId=${post.id}`
       );
       const data = await response.json();
       setComments(data);
     };
     initcommentData();
     console.log(comments);
-  }, [comments]);
+  }, []);
   return (
     <article className="article">
-      <h1 className="article-title">제목 : {post.title} </h1>
-      <span className="article-body">내용 : {post.body}</span>
+      <h1 className="article-title">[ {post.title} ]</h1>
+      <span className="article-body">CONTENTS : {post.body}</span>
+      <h3 className="comment-title">[comments]</h3>
       <section>
         {comments &&
           comments.map((comment) => {
             return (
               <div key={comment.id}>
                 <div>
-                  <h2>댓글</h2>
-                  <h3>
+                  <h5>
                     {comment.name} {comment.email}
-                  </h3>
+                  </h5>
                   <span>{comment.body}</span>
                 </div>
               </div>
