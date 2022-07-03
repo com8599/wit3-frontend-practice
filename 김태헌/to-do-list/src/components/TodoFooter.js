b00d9890b73fca15e5c677e608a0d0d231a61b0c
김태헌 <57055730+Jake-huen@users.noreply.github.com>
2022-07-04 00:55:02 +0900

    contextAPI 사용해보기
    velopert 참고

--- 김태헌/to-do-list/src/components/TodoFooter.js
+++ 김태헌/to-do-list/src/components/TodoFooter.js
@@ -6,13 +6,14 @@ const AddButton = styled.button`
   background: #38d9a9;
   display: flex;
   cursor: pointer;
-  width: 80px;
-  height: 80px;
+  width: 60px;
+  height: 60px;
+  z-index: 1;
   align-items: center;
   justify-content: center;
   position: absolute;
+  bottom: -30px;
   left: 40%;
-  bottom: 0;
   color: white;
   border-radius: 50%;
   border: none;
@@ -21,8 +22,9 @@ const AddButton = styled.button`
   &:active {
     background: #20c997;
   }
+  transition: 0.2s all ease-in;
   ${(props) =>
-    props.checked &&
+    props.clicked &&
     css`
       background: #ff6b6b;
       &:active {
@@ -31,16 +33,44 @@ const AddButton = styled.button`
       transform: rotate(45deg);
     `}
 `;
-
+const InsertTodo = styled.div`
+  width: 100%;
+  bottom: 0;
+  left: 0;
+`;
+const InsertForm = styled.form`
+  background: #f8f9fa;
+  padding: 32px 32px 32px 72px;
+  border-bottom-left-radius: 16px;
+  border-bottom-right-radius: 16px;
+  border-top: 1px solid #e9ecef;
+`;
+const Input = styled.input`
+  padding: 12px;
+  border-radius: 4px;
+  border: 1px solid #dee2e6;
+  width: 100%;
+  font-size: 18px;
+  box-sizing: border-box;
+`;
 const TodoFooter = () => {
   const [clicked, setClicked] = useState(false);
   const onToggle = () => {
     setClicked(!clicked);
   };
   return (
-    <AddButton onClick={onToggle} clicked={clicked}>
-      <MdAdd />
-    </AddButton>
+    <div>
+      {clicked && (
+        <InsertTodo>
+          <InsertForm>
+            <Input placeholder="할 일을 입력 후, Enter를 누르세요" />
+          </InsertForm>
+        </InsertTodo>
+      )}
+      <AddButton onClick={onToggle} clicked={clicked}>
+        <MdAdd />
+      </AddButton>
+    </div>
   );
 };
 
