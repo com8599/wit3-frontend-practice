79deee879b82c8c2b0749f914aa8c14f139404ba
serin301 <najin0250@gmail.com>
2022-08-01 20:22:21 +0900

    fakestore 1차

---
+++ 권세린/fakestore/components/header_box/header.style.ts
@@ -0,0 +1,34 @@
+import styled from 'styled-components';
+
+export const HeaderWrapper = styled.div`
+    width: 100%;
+    height: auto;
+    padding: 3% auto;
+`;
+
+export const BrandName = styled.p`
+    text-align: center;
+    font-weight: bold;
+    font-size: 40px;
+    font-family: 'Permanent Marker', cursive;
+`;
+
+export const Nav = styled.div`
+    width: 100%;
+    height: auto;
+    padding: 3%;
+    text-align: right;
+    background-color: black;
+    color: white;
+`;
+
+export const ShoppingCartLink = styled.a`
+    width: 20%;
+    height: auto;
+    font-size: 15px;
+
+    &:hover{
+        text-decoration: underline;
+        cursor: pointer;
+    }
+`
---
+++ 권세린/fakestore/components/layout/layout_style.ts
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const LayoutWrapper = styled.main`
+  width:100%;
+  height:100%;
+  max-width:480px;
+  margin: 0 auto;
+  background-color: #dddddd;
+ `
